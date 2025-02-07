4975cd0650a3374282647414eaac3db3b64bdada
zhlynn <zhlynn@126.com>
2025-02-07 17:43:06 +0800

    rename JValue to jvalue

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# JValue
+# jvalue
 TODO
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,13 @@
 
 int main(int argc, char *argv[])
 {
-    JValue jv;
+    jvalue jv;
     jv["happy"] = true;
     jv["name"] = "Niels";
     jv["answer"]["everything"] = 66;
 
-    printf("%s\n", jv.styleWrite().c_str());
-    printf("%s\n", jv.writePList().c_str());
+    printf("%s\n", jv.style_write().c_str());
+    printf("%s\n", jv.style_write().c_str());
 
     return 0;
 }
